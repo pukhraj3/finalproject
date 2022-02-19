b27314aa70e743f478e90635dc316c2b92873466
Pukhraj Sidhu <pukhraj@pukhrajs-mac.local>
2022-02-18 23:56:25 -0800

    fp2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { Fab } from "@mui/material/Fab";
+import Card from '@mui/material/Card';
 
 function App() {
   return (
     <div className="App">
-
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     </div>
   );
 }
